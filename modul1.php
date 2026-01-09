6abc40d249541318c27bafd072aaabfd74f9afd9
nogicn <11751305+nogicn@users.noreply.github.com>
2026-01-09 22:02:05 +0100

    add dockerfile

--- modul1.php
+++ modul1.php
@@ -8,7 +8,9 @@
   <meta name="viewport" content="width=device-width", initial-scale=1>
   <link rel="stylesheet" href="main.css" type="text/css">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://code.jquery.com/jquery-3.7.1.js"
+			  integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4="
+			  crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
   <link rel="preconnect" href="https://fonts.gstatic.com/%22%3E">
